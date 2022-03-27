80d19a9477669634b3a57b2e6878e90fce94a9c2
guldan <gulum1111@gmail.com>
2022-03-26 21:54:57 -0400

    Group 10 Project 3 Tasks

---
+++ CarTester.java
@@ -0,0 +1,14 @@
+package com.syntax.groupproject3;
+
+public class CarTester {
+    public static void main(String[] args) {
+        Car s = new Sedan (10000.00, "white", 30);
+        System.out.println(s.calculateSalePrice());
+
+        Car t = new Truck (20000.00, "Black", 3000.0);
+        System.out.println(t.calculateSalePrice());
+
+    }
+}
+
+
